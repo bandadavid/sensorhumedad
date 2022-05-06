c7be305aa61208f4a5414c6ceb0e8b37b6abbc24
Bryancuji <bryan.cuji8748@utc.edu.ec>
2022-05-05 22:30:25 -0500

    Correccion a la opcion de reconocimiento

--- app/src/main/java/com/davidbanda/modulosuelo/ColorSueloActivity.java
+++ app/src/main/java/com/davidbanda/modulosuelo/ColorSueloActivity.java
@@ -2,73 +2,89 @@ package com.davidbanda.modulosuelo;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.palette.graphics.Palette;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ColorSueloActivity extends AppCompatActivity {
 
-    private TextView txt1,txt2,txt3,txt4,txt5;
+    //Definiendo objetos para capturar imagenes
 
-    //Declaracion de variables JAVA para la camara
-    Button btnCamara;
-    ImageView imgView;
-    //Fin de la Declaracion de variables JAVA para la camara
+    private TextView txt1,txt2,txt3,txt4,txt5;
+    ImageView imgFoto;
+    String rutaImagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_suelo);
 
-        //Mapeo de elementos XML a Objetos JAVA
-        btnCamara = findViewById(R.id.btnCamara);
-        imgView = findViewById(R.id.imageView);
-        //Fin de Mapeo de elementos XML a Objetos JAVA
+        //Mapeo de elementos XML a Objeto Java
+        imgFoto=findViewById(R.id.imgFoto);
 
-        //Agregar el evento para el  click del boton
-        btnCamara.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                abrirCamara();
+    }
+
+    //Metodo para crear una imagen
+    public File crearImagen() throws IOException {
+        String nombreImagen="foto_";//definiendo un estandar para el nombre de nuestras imagenes
+        File carpeta=getExternalFilesDir(Environment.DIRECTORY_PICTURES);//definiendo la ruta donde se guardaran las imagenes
+        File imagenTemporal=File.createTempFile(nombreImagen,".jpg",carpeta); //creando la imagen temporal
+        rutaImagen=imagenTemporal.getAbsolutePath();//Obteniendo la ruta de la imagen creada
+        return imagenTemporal;//retornando la imagen temporal que se ha creado
+    }
+    //Metodo para capturar una imagen al dar click en el boton correspondiente
+    public void capturarImagen(View vista){
+        Intent ventanaCamara= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//Creando una actividad para acceder a la camara
+        if(ventanaCamara.resolveActivity(getPackageManager())!=null){
+            //continuar con el proceso de captura de la imagen
+            File imagenArchivo=null;
+            try{
+                imagenArchivo=crearImagen(); //crear la imagen
+            }catch (Exception ex){
+                Toast.makeText(getApplicationContext(),"Error -> "+ex.toString(),
+                        Toast.LENGTH_SHORT).show();
             }
-        });
-        //Fin de Agregar el evento para el  click del boton
-
-
-
-        init();
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.fondo3);
-        crearPalette(bitmap);
-    }
-
-    //Metodo para abrir la camara
-    private void abrirCamara(){
-        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //if (intent.resolveActivity(getPackageManager()) != null){
-        startActivityForResult(intent,1);
-        //}
-    }
-    //Fin del Metodo para abrir la camara
-
-    //Metodo para poder visualizar la imagen
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imgBitmap = (Bitmap) extras.get("data");
-            imgView.setImageBitmap(imgBitmap);
+            if(imagenArchivo!=null){
+                Uri FotoUri= FileProvider.getUriForFile(this,"com.davidbanda.modulosuelo.fileprovider",imagenArchivo);
+                ventanaCamara.putExtra(MediaStore.EXTRA_OUTPUT,FotoUri);//capturando la imagen almacenada
+                startActivityForResult(ventanaCamara,1);
+            }
+        }else{
+            Toast.makeText(getApplicationContext(),"Error al iniciar cámara",Toast.LENGTH_SHORT).show();
         }
     }
-    //Fin delMetodo para poder visualizar la imagen
 
+    //metodo para presentar la imagen dentro del image view correspondiente
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);//llamando al metodo padre
+        if (requestCode==1 && resultCode==RESULT_OK){//validamos que la captura de la imagen sea exitosa
+            //creando un bitmap mediante la ruta de la imagen creada
+            init();
+            Bitmap bitmap= BitmapFactory.decodeFile(rutaImagen);
+            crearPalette(bitmap);
+            //mensaje para presentar la ruta donde se almacena la imagen
+            Toast.makeText(getApplicationContext(),"RUTA:"+rutaImagen,Toast.LENGTH_SHORT).show();
+            //asignando la imagen capturada como vista previa del ImageView
+            imgFoto.setImageBitmap(bitmap);
+
+
+        }
+    }
 
     private void crearPalette(Bitmap bitmap) {
         Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
